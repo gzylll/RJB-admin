70cdae05cc940c921bb4df00e2c8a47a429a8f73
庭庭ttw <13270335225@163>
2017-06-16 17:30:57 +0800

    添加

--- app/src/main/java/valderfields/rjb_admin/view/AdminActivity.java
+++ app/src/main/java/valderfields/rjb_admin/view/AdminActivity.java
@@ -1,9 +1,13 @@
 package valderfields.rjb_admin.view;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import valderfields.rjb_admin.R;
 
@@ -22,5 +26,16 @@ public class AdminActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.addadmin_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void addAdmin(View v){
+        LayoutInflater inflater = this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_admin,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view);
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+
     }
 }
